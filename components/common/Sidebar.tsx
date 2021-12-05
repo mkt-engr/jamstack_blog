83d246ba5f835c4d21356a7fdccdcc0b11e3f027
Makito Mori <mkt.it.engr@gmail.com>
2021-12-05 17:57:34 +0900

    サイドバー作成

--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -1,7 +1,28 @@
 import { VFC } from "react";
+const categoryList: string[] = [
+  "TypeScript",
+  "React",
+  "Vue",
+  "HTML",
+  "SCSS",
+  "CSS",
+];
 
 const Sidebar: VFC = () => {
-  return <div className="w-250 bg-green-500 hidden lg:block">Sidebar.tsx</div>;
+  console.log({ categoryList });
+  return (
+    <div className="p-2 w-250  hidden lg:block">
+      <div className="p-3 rounded bg-gray-300">
+        <h2 className="font-semibold">Category</h2>
+        <ul>
+          {categoryList.map((category, index) => {
+            console.log(category);
+            return <li key={index.toString()}>{category}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
