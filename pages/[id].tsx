3d3d851b02a318f9c51287bf583397a7d9804127
Makito Mori <mkt.it.engr@gmail.com>
2021-12-17 00:00:07 +0900

    Prismでハイライトできた 後は行番号。。

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -6,13 +6,15 @@ import { formatYYYYMMDDdd } from "../lib/dayjs";
 import cheerio from "cheerio";
 import hljs from "highlight.js";
 import "highlight.js/styles/hybrid.css";
-import Prism from "prismjs";
+import Prism from "prismjs"; // step1
+import "prismjs/themes/prism.css"; // step2
 
 const Blog = ({ article }) => {
   const { title, body, createdAt, updatedAt } = article;
   React.useEffect(() => {
-    Prism.highlightAll();
-  }, []);
+    console.log("1");
+    Prism.highlightAll(); // step3
+  });
   return (
     <ArticleLayout title={title}>
       <div className="p-4 md:p-12 bg-white rounded">
@@ -42,17 +44,46 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const article = await getArticleById(params.id);
   const $ = cheerio.load(article.body);
-  console.log({ $ });
-  $("pre code").each((_, elm) => {
-    const result = hljs.highlightAuto($(elm).text());
-    $(elm).html(result.value);
-    $(elm).addClass("hljs");
-  });
+  //highlight.js
+  // $("pre code").each((_, elm) => {
+  //   const result = hljs.highlightAuto($(elm).text());
+  //   $(elm).html(result.value);
+  //   $(elm).addClass("hljs");
+  // });
+
+  //Prism.js
+  // $("pre").each((_, elm) => {
+  //   $(elm).addClass("language-javascript");
+  // });
   // const html = Prism.highlight(
   //   article.body,
   //   Prism.languages.javascript,
   //   "javascript"
   // );
+
+  // https://github.com/PrismJS/prism/blob/master/plugins/line-numbers/prism-line-numbers.js#L109
+  var NEW_LINE_EXP = /\n(?!$)/g;
+  var lineNumbersWrapper;
+
+  Prism.hooks.add("after-tokenize", function (env) {
+    var match = env.code.match(NEW_LINE_EXP);
+    var linesNum = match ? match.length + 1 : 1;
+    var lines = new Array(linesNum + 1).join("<span></span>");
+
+    lineNumbersWrapper = `<span aria-hidden="true" class="line-numbers-rows">${lines}</span>`;
+  });
+
+  $("pre code").each((_, elm) => {
+    // const result = hljs.highlightAuto($(elm).text());
+    const a = Prism.highlight(
+      $(elm).text(),
+      Prism.languages.javascript,
+      "javascript"
+    );
+    // $(elm).html(result.value);
+    $(elm).html(a + lineNumbersWrapper);
+    // $(elm).addClass("hljs");
+  });
   return {
     props: {
       // article,
