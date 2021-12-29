d597bfc7e56ffe2fc30d513e2074fd155d6ae617
Makito Mori <mkt.it.engr@gmail.com>
2021-12-29 23:33:42 +0900

    ページ遷移時にもGAを発火させる

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { AppProps } from "next/app";
+import * as gtag from "../lib/gtag";
 import "../styles/globals.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  // Google Analyticsをページ遷移時にも対応させる
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 };
 
