e2c5a95583757c341f525ef376dd1b60fac069aa
Makito Mori <mkt.it.engr@gmail.com>
2022-01-30 23:59:20 +0900

    フッターの位置を決める必要がなかったのでhookやCSSを削除

--- components/top/Layout.tsx
+++ components/top/Layout.tsx
@@ -1,26 +1,19 @@
-import { ReactNode, VFC, useEffect } from "react";
+import { ReactNode, VFC } from "react";
 import Head from "next/head";
 import Header from "../common/Header";
 import Footer from "../common/Footer";
-import styles from "./Layout.module.scss";
-import { useSetFooterPosition } from "../../hooks/useSetFooterPosition";
+
 interface Props {
   children: ReactNode;
   title?: string;
 }
 const Layout: VFC<Props> = ({ children, title }) => {
-  //フッターの位置調整
-  useSetFooterPosition();
-
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
-      <div
-        className={`flex bg-gray-100 flex-col ${styles.contentWrapper}`}
-        id="js-contentWrapper"
-      >
+      <div className={`flex bg-gray-100 flex-col h-screen`}>
         <Header />
         <div className="flex-1 px-4 md:px-18 xl:px-36 bg-gray-100 blogContent">
           <main>{children}</main>
